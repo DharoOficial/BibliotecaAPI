f6342dd4414b65d25e0b22cfe1d579f215b4799f
DharoOficial <erickbandeiradharo@gmail.com>
2024-08-07 13:33:55 -0300

    Adicionando util CreateFiles

---
+++ BibliotecaAPI/MVCAPIBiblioteca/Utils/CreateNewFile.cs
@@ -0,0 +1,17 @@
+﻿using System.Text;
+
+namespace MVCAPIBiblioteca.Utils
+{
+    public class CreateNewFile <T>
+    {
+        public CreateNewFile(string pathFile, string content)
+        {
+            FileStream fileStream = new FileStream(pathFile, FileMode.Create);
+            using (var escritor = new StreamWriter(fileStream))
+            {
+                escritor.WriteLine(content);
+
+            }
+        }
+    }
+}
